d93d0205caad394bb61aa805bc0f32e2c5ee1345
ThiagoDe <17tawi@gmail.com>
2022-05-18 11:18:55 -0700

    Largest Component

---
+++ largestComponent.js
@@ -0,0 +1,21 @@
+onst largestComponent = (graph) => {
+  const visited = new Set();
+  let max = 0;
+  
+  for (let node in graph){
+    max = Math.max(max, explore(graph, node, visited))
+  }
+  
+  return max
+}
+
+const explore = (graph, node, visited) => {
+  if (visited.has(String(node))) return 0;
+  visited.add(String(node));
+  
+  let count = 1;
+  for (let neighbor of graph[node]){
+    count += explore(graph, neighbor, visited)
+  }
+  return count;
+}
