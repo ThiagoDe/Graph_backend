4e2411901fb3d042323172ba65c897cca838f5e3
ThiagoDe <17tawi@gmail.com>
2022-05-17 22:43:40 -0700

    stack depth first

---
+++ introGraph.js
@@ -0,0 +1,17 @@
+//  const dfp = (graph, src) => {
+//      const stack = [ src ]
+
+//      while (stack.length > 0) {
+//          const current = stack.pop();
+
+//          for (let neighbor of graph[current]){
+//              stack.push(neighbor);
+//          }
+//      }
+//  }
+
+const dft = (graph, src) => {
+   for (neighbor of graph[src]) {
+       dft(graph, neighbor)
+   }
+}
